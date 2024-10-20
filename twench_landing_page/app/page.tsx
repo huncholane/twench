30f7b847e34cd49f438f1ca46af8be705d99e969
Lane Henslee <huncho@hygo.ai>
2024-10-20 12:12:37 -0700

    using components folder and fixed text shadow

--- twench_landing_page/app/page.tsx
+++ twench_landing_page/app/page.tsx
@@ -1,54 +1,16 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
       <div className="landing flex flex-col">
-        <div className="h-32 flex justify-between">
-          <div></div>
-          <Image
-            src="/images/logo.png"
-            alt="Twench"
-            width={64}
-            height={64}
-            className="my-auto rounded-full border-black border-4"
-          />
-          <div className="flex gap-4 my-auto">
-            <a
-              href=""
-              className="flex h-10 w-10 justify-center bg-white border-black border-2"
-            >
-              <Image
-                src="/images/dexscreener.png"
-                width={25}
-                height={30}
-                alt="Dexscreener"
-                className="my-auto"
-              />
-            </a>
-            <a
-              href=""
-              className="flex h-10 w-10 justify-center bg-white border-black border-2"
-            >
-              <Image
-                src="/images/dextools.png"
-                width={25}
-                height={30}
-                alt="Dexscreener"
-                className="my-auto"
-              />
-            </a>
-          </div>
-          <a
-            className="p-3 border-black bg-background my-auto rounded-full border-4 text-[28px] leading-[30.52px] cursor-pointer text-shadow"
-            href=""
-          >
-            BUY $TWENCH
-          </a>
-          <div className="h-full"></div>
+        <Header />
+        <div className="relative">
+          <h1 className="text-[128px] large-text-shadow w-full text-center">
+            $TWENCH
+          </h1>
         </div>
       </div>
-      <div></div>
       <div></div>
       <div></div>
     </div>
---
+++ twench_landing_page/components/Header.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="h-32 flex justify-between">
+      <div></div>
+      <Image
+        src="/images/logo.png"
+        alt="Twench"
+        width={64}
+        height={64}
+        className="my-auto rounded-full border-black border-4"
+      />
+      <div className="flex gap-4 my-auto">
+        <a
+          href=""
+          className="flex h-10 w-10 justify-center bg-white border-black border-2"
+        >
+          <Image
+            src="/images/dexscreener.png"
+            width={25}
+            height={30}
+            alt="Dexscreener"
+            className="my-auto"
+          />
+        </a>
+        <a
+          href=""
+          className="flex h-10 w-10 justify-center bg-white border-black border-2"
+        >
+          <Image
+            src="/images/dextools.png"
+            width={25}
+            height={30}
+            alt="Dexscreener"
+            className="my-auto"
+          />
+        </a>
+      </div>
+      <a
+        className="p-3 border-black bg-background my-auto rounded-full border-4 text-[28px] leading-[30.52px] cursor-pointer"
+        href=""
+      >
+        BUY $TWENCH
+      </a>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
