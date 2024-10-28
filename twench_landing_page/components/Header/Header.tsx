62e1f30dd70a8b39e2c231c91ee52bce253b110b
Lane Henslee <huncho@hygo.ai>
2024-10-28 05:03:58 -0700

    header buy button implements new text shadow

--- twench_landing_page/components/Header/Header.tsx
+++ twench_landing_page/components/Header/Header.tsx
@@ -40,10 +40,7 @@ const Header = () => {
           />
         </a>
       </div>
-      <a
-        className="p-1 sm:p-3 border-black bg-background my-auto rounded-full border-4 text-[12px] leading-[30.52px] cursor-pointer hover:brightness-150 sm:text-[28px] mr-3 sm:mr-0 small-text-shadow sm:text-shadow text-white"
-        href=""
-      >
+      <a className="HeaderBuyButton" href="">
         BUY $TWENCH
       </a>
       <div className="hidden sm:block"></div>
