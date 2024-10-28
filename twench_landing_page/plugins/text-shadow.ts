55a1b671c58a539220cd89ed73590bb163c1b3d2
Lane Henslee <huncho@hygo.ai>
2024-10-28 05:24:24 -0700

    text shadow plugin is easy to import

--- twench_landing_page/plugins/text-shadow.ts
+++ twench_landing_page/plugins/text-shadow.ts
@@ -1,17 +1,37 @@
+import { text } from "stream/consumers";
 import plugin from "tailwindcss/plugin";
 
-const textShadowPlugin = plugin(function ({ matchUtilities, theme }) {
-  matchUtilities(
-    {
-      "text-shadow": (value: string) => ({
-        textShadow: `${value} ${value} 0 #000, -${value} -${value} 0 #000, ${value} -${value} 0 #000,
-        -${value} ${value} 0 #000, ${value} 0 0 #000, -${value} 0 0 #000, 0 ${value} 0 #000, 0 -${value} 0 #000;`,
-      }),
+function textShadow(value: string) {
+  return {
+    textShadow: `${value} ${value} 0 #000, -${value} -${value} 0 #000, ${value} -${value} 0 #000,
+    -${value} ${value} 0 #000, ${value} 0 0 #000, -${value} 0 0 #000, 0 ${value} 0 #000, 0 -${value} 0 #000;`,
+  };
+}
+
+const textShadowPlugin = plugin(
+  function ({ matchUtilities, theme }) {
+    matchUtilities(
+      {
+        "text-shadow": (value: string) => ({
+          textShadow: textShadow(value),
+        }),
+      },
+      {
+        values: theme("textShadow"),
+      }
+    );
+  },
+  {
+    theme: {
+      textShadow: {
+        DEFAULT: "1px",
+        sm: "2px",
+        md: "3px",
+        lg: "4px",
+        xl: "5px",
+      },
     },
-    {
-      values: theme("textShadow"),
-    }
-  );
-});
+  }
+);
 
 export default textShadowPlugin;
--- twench_landing_page/tailwind.config.ts
+++ twench_landing_page/tailwind.config.ts
@@ -13,13 +13,6 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      textShadow: {
-        DEFAULT: "1px",
-        sm: "2px",
-        md: "3px",
-        lg: "4px",
-        xl: "5px",
-      },
     },
   },
   plugins: [textShadowPlugin],
