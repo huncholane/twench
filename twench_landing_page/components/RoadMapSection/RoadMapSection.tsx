9cc632a7c59e043f7bd7e1cf73ca23bcec1d89e9
Lane Henslee <huncho@hygo.ai>
2024-10-29 02:44:09 -0700

    pictures laid out for roadmap

--- twench_landing_page/components/RoadMapSection/RoadMapSection.tsx
+++ twench_landing_page/components/RoadMapSection/RoadMapSection.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './RoadMapSection.css';
+import React from "react";
+import "./RoadMapSection.css";
 
 const RoadMapSection: React.FC = () => {
   return (
     <div className="RoadMapSection">
-      RoadMapSection
+      <div className="Container">
+        <div className="Title">ROAD MAP</div>
+        <div className="Soldier"></div>
+        <div className="Tnt"></div>
+        <div className="Jetpack"></div>
+        <div className="Wallstreet"></div>
+      </div>
     </div>
   );
 };
