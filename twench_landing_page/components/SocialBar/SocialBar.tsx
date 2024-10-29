2c5754d040a02dcef6008da97fbf52d59e3d16fa
Lane Henslee <huncho@hygo.ai>
2024-10-29 05:37:14 -0700

    running guy never gets too big

--- twench_landing_page/components/SocialBar/SocialBar.tsx
+++ twench_landing_page/components/SocialBar/SocialBar.tsx
@@ -4,13 +4,21 @@ import "./SocialBar.css";
 const SocialBar: React.FC = () => {
   return (
     <div className="SocialBar">
-      <div className="Twitter"></div>
-      <div className="Telegram"></div>
-      <div className="Youtube"></div>
-      <div className="Instagram"></div>
-      <div className="TikTok"></div>
-      <div className="Instagram"></div>
-      <div className="SoundCloud"></div>
+      <a
+        href="https://x.com/Twench_sol"
+        target="_blank"
+        className="Twitter"
+      ></a>
+      <a
+        href="https://t.me/+CsIy0xZ2nsk3Yzhi"
+        target="_blank"
+        className="Telegram"
+      ></a>
+      <a className="Youtube"></a>
+      <a className="Instagram"></a>
+      <a className="TikTok"></a>
+      <a className="Instagram"></a>
+      <a className="SoundCloud"></a>
     </div>
   );
 };
