a462a0586dc750f0c1bfc4413749d52cddcdb188
Lane Henslee <huncho@hygo.ai>
2024-10-29 05:03:22 -0700

    video section goes to full screen

--- twench_landing_page/components/VideoSection/VideoSection.tsx
+++ twench_landing_page/components/VideoSection/VideoSection.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import "./VideoSection.css";
 
 const VideoSection: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleVideoClick = () => {
+    if (videoRef.current) {
+      if (videoRef.current.requestFullscreen) {
+        videoRef.current.requestFullscreen();
+      }
+    }
+  };
+
   return (
     <div className="VideoSection">
       <div className="Preview">
-        <button></button>
+        <video
+          ref={videoRef}
+          src="/videos/v2.mp4"
+          controls
+          onClick={handleVideoClick}
+        ></video>
       </div>
     </div>
   );
