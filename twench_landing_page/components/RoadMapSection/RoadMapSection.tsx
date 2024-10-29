cf3396de883ea74bb82e0c55b71e3345e7746332
Lane Henslee <huncho@hygo.ai>
2024-10-29 04:45:12 -0700

    roadmap a little responsive

--- twench_landing_page/components/RoadMapSection/RoadMapSection.tsx
+++ twench_landing_page/components/RoadMapSection/RoadMapSection.tsx
@@ -7,34 +7,34 @@ const RoadMapSection: React.FC = () => {
       <div className="Container">
         <div className="Title">ROAD MAP</div>
         <div className="Row">
+          <div className="Soldier"></div>
           <ul>
             <li>Build Community Website & Social Media Accounts</li>
             <li>Build Twench Run GAME</li>
             <li>Launch On PumpFun</li>
           </ul>
-          <div className="Soldier"></div>
         </div>
         <div className="Row">
-          <div className="Tnt"></div>
           <ul>
             <li>Hard Work In Da Twench</li>
             <li>Migrate to Raydium</li>
             <li>Strategic Partnerships</li>
             <li>Twench Run Expansions</li>
           </ul>
+          <div className="Tnt"></div>
         </div>
         <div className="Row">
+          <div className="Jetpack"></div>
           <ul>
             <li>Growth & Community Rewards</li>
             <li>Expand Listings</li>
           </ul>
-          <div className="Jetpack"></div>
         </div>
         <div className="Row">
-          <div className="Wallstreet"></div>
           <ul>
             <li>GET OUT DA TWENCH</li>
           </ul>
+          <div className="Wallstreet"></div>
         </div>
       </div>
     </div>
