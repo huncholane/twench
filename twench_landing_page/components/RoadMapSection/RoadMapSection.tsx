26abf4646d2ee4f7de8dbaac542857343e5933d9
Lane Henslee <huncho@hygo.ai>
2024-10-29 04:20:16 -0700

    added roadmap text

--- twench_landing_page/components/RoadMapSection/RoadMapSection.tsx
+++ twench_landing_page/components/RoadMapSection/RoadMapSection.tsx
@@ -6,10 +6,36 @@ const RoadMapSection: React.FC = () => {
     <div className="RoadMapSection">
       <div className="Container">
         <div className="Title">ROAD MAP</div>
-        <div className="Soldier"></div>
-        <div className="Tnt"></div>
-        <div className="Jetpack"></div>
-        <div className="Wallstreet"></div>
+        <div className="Row">
+          <ul>
+            <li>Build Community Website & Social Media Accounts</li>
+            <li>Build Twench Run GAME</li>
+            <li>Launch On PumpFun</li>
+          </ul>
+          <div className="Soldier"></div>
+        </div>
+        <div className="Row">
+          <div className="Tnt"></div>
+          <ul>
+            <li>Hard Work In Da Twench</li>
+            <li>Migrate to Raydium</li>
+            <li>Strategic Partnerships</li>
+            <li>Twench Run Expansions</li>
+          </ul>
+        </div>
+        <div className="Row">
+          <ul>
+            <li>Growth & Community Rewards</li>
+            <li>Expand Listings</li>
+          </ul>
+          <div className="Jetpack"></div>
+        </div>
+        <div className="Row">
+          <div className="Wallstreet"></div>
+          <ul>
+            <li>GET OUT DA TWENCH</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
